ec31f7cb37e3f4546b09d28e1207a60d682119bc
Max <idhdb1004@naver.com>
2024-06-02 15:05:03 +0900

    TIR

--- Helloworld/FileName.cpp
+++ Helloworld/FileName.cpp
@@ -73,7 +73,7 @@ return type function-name (parameters)
 	statements
 }
 
-*/
+
 
 #include <stdbool.h>
 #include <stdio.h>
@@ -110,3 +110,74 @@ int main(void) {
 	}
 	return 0;
 }
+
+
+
+#include <stdio.h>
+#include <stdbool.h>
+
+bool is_prime(int n)
+{
+	int divisor;
+
+	if (n <= 1)
+		return false;
+	for (divisor = 2; divisor * divisor <= n; divisor++)
+		if (n % divisor == 0)
+			return false;
+	return true;
+}
+
+int main(void)
+{
+	int n;
+
+	printf("Enter a number: ");
+	scanf_s("%d", &n);
+	if (is_prime(n))
+		printf("Prime\n");
+	else
+		printf("Not prime\n");
+	return 0;
+}
+
+
+
+#include <stdio.h>
+
+int power_recursive(int number, int exponent)
+{
+	if (exponent == 0) {
+		return 1;
+	}
+	else {
+		return number * power_recursive(number, exponent - 1);
+	}
+}
+
+
+1 - 한 학생의 국어 성적, 영어 성적, 수학 성적을 관리하는 배열을 선언하세요.
+그리고 초기값으로 국어 성적은 80, 영어 성적은 60, 수학 성적은 90으로 지정하고 각 원소의 값을 출력하세요.
+
+2- 다음의 출력 결과를 예측하시오.
+
+#include <stdio.h>
+int main()
+{
+	int arr[10]={1};
+	int index = 0;
+
+	for(index = 0; index<10; index++)
+	{
+		printf("%d\n",arr[index]);
+	}
+	return 0;
+}
+3- 세 학생의 국어 성적, 영어 성적, 수학 성적을 관리하는 배열을 선언하세요.
+그리고 세 명의 성적을 입력받아 과목별 합계와 평균을 출력하시오.
+그리고 학생별로 합계와 평균을 출력하시오.
+
+*/
+
+#include <stdio.h>
+
